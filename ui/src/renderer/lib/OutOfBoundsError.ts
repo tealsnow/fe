b6f2305cc1292cf0fccf5af972daa090b79427cf
Ketan Reynolds <ketan.cs.reynolds@gmail.com>
2025-10-02 21:33:44 +0200

    remove dead code

--- ui/src/renderer/lib/OutOfBoundsError.ts
+++
@@ -1,9 +0,0 @@
-import { Data } from "effect";
-import Integer from "./Integer";
-
-export class OutOfBoundsError extends Data.TaggedError("OutOfBoundsError")<{
-  index: Integer;
-}> {
-  message = `Index ${this.index} is out of bounds`;
-}
-export default OutOfBoundsError;
